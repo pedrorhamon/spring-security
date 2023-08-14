cf1572bd1ab9648704ec67d5f710ecaf3abc5135
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-08-14 17:33:32 -0300

    class services

---
+++ src/main/java/com/starking/crud/services/UsuarioService.java
@@ -0,0 +1,14 @@
+package com.starking.crud.services;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author pedroRhamon
+ */
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+}
