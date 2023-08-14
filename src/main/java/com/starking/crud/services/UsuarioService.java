9d4c44d1c92b5e084ab8b93ce29ba2b6a4e2e34f
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-08-14 20:54:00 -0300

    Metodo de salvar com Optional e boas praticas services

--- src/main/java/com/starking/crud/services/UsuarioService.java
+++ src/main/java/com/starking/crud/services/UsuarioService.java
@@ -1,6 +1,11 @@
 package com.starking.crud.services;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+
+import com.starking.crud.domain.model.Usuario;
+import com.starking.crud.repositories.UsuarioRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,5 +15,12 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
+	
+	private final UsuarioRepository usuarioRepository;
+	
+	public Optional<Usuario> salvarUsuario(Usuario usuario) {
+		Usuario usuarioSalvo = this.usuarioRepository.save(usuario);
+		return Optional.ofNullable(usuarioSalvo);
+	}
 
 }
