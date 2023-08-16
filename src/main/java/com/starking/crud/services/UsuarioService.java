d0fce3b87a083712733ad7a3ef64df1ed8d09d68
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-08-16 10:11:45 -0300

    criando dois metodos

--- src/main/java/com/starking/crud/services/UsuarioService.java
+++ src/main/java/com/starking/crud/services/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.starking.crud.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.starking.crud.domain.model.Usuario;
 import com.starking.crud.repositories.UsuarioRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -15,9 +17,18 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
-	
+
 	private final UsuarioRepository usuarioRepository;
-	
+
+	public List<Usuario> listarUsuario() {
+		return this.usuarioRepository.findAll();
+	}
+
+	public void buscarPorId(Usuario usuario) {
+		this.usuarioRepository.findById(usuario.getId());
+	}
+
+	@Transactional
 	public Optional<Usuario> salvarUsuario(Usuario usuario) {
 		Usuario usuarioSalvo = this.usuarioRepository.save(usuario);
 		return Optional.ofNullable(usuarioSalvo);
