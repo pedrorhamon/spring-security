a1c9b4ad24a1c1dc04735ba158e95052dde9dda9
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-08-17 15:17:59 -0300

    Iniciando RestApi controller

---
+++ src/main/java/com/starking/crud/controller/UsuarioController.java
@@ -0,0 +1,13 @@
+package com.starking.crud.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pedroRhamon
+ */
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+}
