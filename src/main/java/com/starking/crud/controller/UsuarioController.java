67b5dbd7f9c6ee5b58eb668f8886caa7f1dd5e7e
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-08-17 19:10:54 -0300

    metodo de listar usuario e response ok

--- src/main/java/com/starking/crud/controller/UsuarioController.java
+++ src/main/java/com/starking/crud/controller/UsuarioController.java
@@ -1,13 +1,32 @@
 package com.starking.crud.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.starking.crud.domain.model.Usuario;
+import com.starking.crud.services.UsuarioService;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * @author pedroRhamon
  */
 @RestController
 @RequestMapping("/usuario")
+@RequiredArgsConstructor
 public class UsuarioController {
+	
+	private final UsuarioService usuarioService;
+	
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public List<Usuario> listarUsuario() {
+		return this.usuarioService.listarUsuario();
+	}
 
 }
