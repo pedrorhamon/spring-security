13980abb71f2704da4bb73f700a2b2baebc6d443
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-08-18 20:06:39 -0300

    criando metodo de buscar por id

--- src/main/java/com/starking/crud/controller/UsuarioController.java
+++ src/main/java/com/starking/crud/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,13 +21,19 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/usuario")
 @RequiredArgsConstructor
 public class UsuarioController {
-	
+
 	private final UsuarioService usuarioService;
-	
+
 	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
 	public List<Usuario> listarUsuario() {
 		return this.usuarioService.listarUsuario();
+	}
+
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void buscarPorId(@RequestParam Usuario usuario) {
+		this.usuarioService.buscarPorId(usuario);
 	}
 
 }
