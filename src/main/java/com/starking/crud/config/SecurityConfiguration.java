db83cd5b67557aa82e12e605e27e0c8de806e8d1
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-08-21 20:14:27 -0300

    implementando config Security com spring atualizado

--- src/main/java/com/starking/crud/config/SecurityConfiguration.java
+++ src/main/java/com/starking/crud/config/SecurityConfiguration.java
@@ -1,8 +1,30 @@
 package com.starking.crud.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsUtils;
+
+
 /**
  * @author pedroRhamon
  */
+@EnableWebSecurity
 public class SecurityConfiguration {
+	
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((authz) -> authz
+            		.requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+    				.requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+    				.requestMatchers(HttpMethod.POST, "/api/usuarios/autenticar").permitAll()
+    				.requestMatchers(HttpMethod.POST, "/api/usuarios").permitAll()
+                .anyRequest().authenticated()
+            );
+        return http.build();
+    }
 
 }
