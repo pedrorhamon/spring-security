4b357df8752423a98dd6aabb0975eb970e0bce3e
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-08-22 20:14:30 -0300

    adicionando o Optional no repository Usuario

--- src/main/java/com/starking/crud/repositories/UsuarioRepository.java
+++ src/main/java/com/starking/crud/repositories/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.starking.crud.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.starking.crud.domain.model.Usuario;
@@ -9,5 +11,5 @@ import com.starking.crud.domain.model.Usuario;
  */
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	
-	Usuario findByEmail(String email);
+	Optional<Usuario> findByEmail(String email);
 }
