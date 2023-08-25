945931b86451ec7bed4046466086f57f7b9a9312
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-08-25 20:58:33 -0300

    criando metodo de salvar produto

--- src/main/java/com/starking/crud/controller/ProdutoController.java
+++ src/main/java/com/starking/crud/controller/ProdutoController.java
@@ -1,9 +1,13 @@
 package com.starking.crud.controller;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +32,12 @@ public class ProdutoController {
 	@ResponseStatus(HttpStatus.OK)
 	public Page<Produto> buscarTodosProdutos(Pageable pageable) {
 		return this.produtoService.buscarTodosProduto(pageable);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Optional<Produto> salvarProduto(@RequestBody Produto produto) {
+		return this.produtoService.salvarProduto(produto);
 	}
 
 }
