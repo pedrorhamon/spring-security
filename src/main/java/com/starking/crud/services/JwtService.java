53244e37b53cf7c08ddcef6aa63a6adc76c2bf4f
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-08-25 21:54:03 -0300

    adicionando dependency do jwt

--- src/main/java/com/starking/crud/services/JwtService.java
+++ src/main/java/com/starking/crud/services/JwtService.java
@@ -1,6 +1,16 @@
 package com.starking.crud.services;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Value;
+
+import com.starking.crud.domain.model.Usuario;
+
+import io.jsonwebtoken.SignatureAlgorithm;
 
 /**
  * @author pedroRhamon
@@ -13,4 +23,25 @@ public class JwtService {
 	@Value("${jwt.chave-assinatura}")
 	private String chaveAssinatura;
 
+	public String gerarToken(Usuario usuario) {
+		long exp = Long.valueOf(expiracao);
+		LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(exp);
+		Instant instant = dataHoraExpiracao.atZone( ZoneId.systemDefault() ).toInstant();
+		java.util.Date data = Date.from(instant);
+		
+		String horaExpiracaoToken = dataHoraExpiracao.toLocalTime()
+				.format(DateTimeFormatter.ofPattern("HH:mm"));
+		
+		String token = Jwts
+							.builder()
+							.setExpiration(data)
+							.setSubject(usuario.getEmail())
+							.claim("userid", usuario.getId())
+							.claim("nome", usuario.getNome())
+							.claim("horaExpiracao", horaExpiracaoToken)
+							.signWith( SignatureAlgorithm.HS512 , chaveAssinatura )
+							.compact();
+		
+		return token;
+	}
 }
