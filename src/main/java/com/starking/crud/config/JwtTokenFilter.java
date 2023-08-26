6b5fc37242da149e8926e641aa28c965e69f5bf3
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-08-25 22:02:18 -0300

    implementação do filter Token

--- src/main/java/com/starking/crud/config/JwtTokenFilter.java
+++ src/main/java/com/starking/crud/config/JwtTokenFilter.java
@@ -2,23 +2,57 @@ package com.starking.crud.config;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.starking.crud.services.JwtService;
+import com.starking.crud.services.SecurityUserDetailsService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 /**
  * @author pedroRhamon
  */
+@RequiredArgsConstructor
 public class JwtTokenFilter extends OncePerRequestFilter {
+
+	private final JwtService jwtService;
+	private final SecurityUserDetailsService userDetailsService;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		
-	}
+		String authorization = request.getHeader("Authorization");
 
+		// "Bearer","eyJhbGciOiJIUzUxMiJ9.eyJ..."
+
+		if (authorization != null && authorization.startsWith("Bearer")) {
+
+			String token = authorization.split(" ")[1];
+			boolean isTokenValid = jwtService.isTokenValido(token);
+
+			if (isTokenValid) {
+				String login = jwtService.obterLoginUsuario(token);
+				UserDetails usuarioAutenticado = userDetailsService.loadUserByUsername(login);
+
+				UsernamePasswordAuthenticationToken user = new UsernamePasswordAuthenticationToken(usuarioAutenticado,
+						null, usuarioAutenticado.getAuthorities());
+
+				user.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+				SecurityContextHolder.getContext().setAuthentication(user);
+
+			}
+		}
+
+		filterChain.doFilter(request, response);
+	}
 }
