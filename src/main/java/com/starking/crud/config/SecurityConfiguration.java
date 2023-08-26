5b4fcedcf5ed220a968cdef70dd91b57be80e669
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-08-25 22:07:13 -0300

    ajustando metodo

--- src/main/java/com/starking/crud/config/SecurityConfiguration.java
+++ src/main/java/com/starking/crud/config/SecurityConfiguration.java
@@ -1,14 +1,24 @@
 package com.starking.crud.config;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.starking.crud.services.JwtService;
 import com.starking.crud.services.SecurityUserDetailsService;
@@ -46,18 +56,49 @@ public class SecurityConfiguration {
 	}
 	
 	@Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception, IllegalArgumentException {
         http
-            .authorizeHttpRequests((authz) -> authz
-            		.requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-    				.requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-    				.requestMatchers(HttpMethod.POST, "/api/usuarios/autenticar").permitAll()
-    				.requestMatchers(HttpMethod.POST, "/api/usuarios").permitAll()
-                .anyRequest().authenticated()
+            .authorizeHttpRequests((authz) -> {
+				try {
+					authz
+							.requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+							.requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+							.requestMatchers(HttpMethod.POST, "/api/usuarios/autenticar").permitAll()
+							.requestMatchers(HttpMethod.POST, "/api/usuarios").permitAll()
+					    .anyRequest().authenticated()
+					    .and()
+						.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+					.and()
+						.addFilterBefore( jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class );
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
             );
         return http.build();
     }
 	
-	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		
+		List<String> all = Arrays.asList("*");
+		
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedMethods(all);
+		config.setAllowedOrigins(all);
+		config.setAllowedHeaders(all);
+		config.setAllowCredentials(true);
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		
+		CorsFilter corFilter = new CorsFilter(source);
+		
+		FilterRegistrationBean<CorsFilter> filter = 
+				new FilterRegistrationBean<CorsFilter>(corFilter);
+		filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		
+		return filter;
+	}
 
 }
