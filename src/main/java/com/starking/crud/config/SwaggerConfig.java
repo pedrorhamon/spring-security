1b3096026398a2f85fa278bffc55c2e3e3c39b75
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-09-01 20:22:31 -0300

    iniciando class com as anotações padroes

--- src/main/java/com/starking/crud/config/SwaggerConfig.java
+++ src/main/java/com/starking/crud/config/SwaggerConfig.java
@@ -1,8 +1,15 @@
 package com.starking.crud.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
  * @author pedroRhamon
  */
-public class SwaggerConfig {
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig implements WebMvcConfigurer {
 
 }
