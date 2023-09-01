b34c5613b11eeaea54c8793465026242c422f74b
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-09-01 20:24:19 -0300

    addResourceHandlers

--- src/main/java/com/starking/crud/config/SwaggerConfig.java
+++ src/main/java/com/starking/crud/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.starking.crud.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,5 +12,19 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig implements WebMvcConfigurer {
+
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry
+                .addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+	
+	
 
 }
