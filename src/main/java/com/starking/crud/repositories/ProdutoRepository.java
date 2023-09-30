90e1f0d1c5b874967d2f30d22de3e808d22ed5e3
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-09-30 20:33:17 -0300

    o eclipse ta bugado não consegue compilar o .m2, depois vejo esse erro

--- src/main/java/com/starking/crud/repositories/ProdutoRepository.java
+++ src/main/java/com/starking/crud/repositories/ProdutoRepository.java
@@ -1,12 +1,10 @@
 package com.starking.crud.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.starking.crud.domain.model.Produto;
 
 /**
  * @author pedroRhamon
  */
-@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{}
